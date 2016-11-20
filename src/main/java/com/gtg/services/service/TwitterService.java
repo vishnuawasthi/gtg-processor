cbf736ee49568288f8720f0287618a071193aac9
vishnuawasthi <vishnuawasthi121@gmail.com>
2016-11-20 16:44:24 +0530

    commented redis config

---
+++ src/main/java/com/gtg/services/service/TwitterService.java
@@ -0,0 +1,5 @@
+package com.gtg.services.service;
+
+public interface TwitterService {
+
+}
---
+++ src/main/java/com/gtg/services/service/TwitterServiceImpl.java
@@ -0,0 +1,11 @@
+package com.gtg.services.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TwitterServiceImpl implements TwitterService {
+
+	
+	
+	
+}
